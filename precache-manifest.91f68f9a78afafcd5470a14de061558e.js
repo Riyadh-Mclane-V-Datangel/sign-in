446c987d991ed2b6cd0747d5df0ae8f57563090e
mclane <mclane0618@gmail.com>
2024-05-15 14:07:51 +0800

    Updates

---
+++ precache-manifest.91f68f9a78afafcd5470a14de061558e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70cc9c604fa5bc7210512037e1fee208",
+    "url": "/sign-in/index.html"
+  },
+  {
+    "revision": "4f5921f2055e5c7c0b4d",
+    "url": "/sign-in/static/css/main.1e23a323.chunk.css"
+  },
+  {
+    "revision": "df2e1980190ef56b300b",
+    "url": "/sign-in/static/js/2.734bbb90.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/sign-in/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "4f5921f2055e5c7c0b4d",
+    "url": "/sign-in/static/js/main.6732baba.chunk.js"
+  },
+  {
+    "revision": "c88bfa0b867c8e2aedf0",
+    "url": "/sign-in/static/js/runtime~main.71a58542.js"
+  },
+  {
+    "revision": "a838133e540468c71092d9a071f5e7c8",
+    "url": "/sign-in/static/media/InterTight-Light.a838133e.ttf"
+  },
+  {
+    "revision": "f1d44d5a53f2b7de517cbea3bd0b272e",
+    "url": "/sign-in/static/media/background.f1d44d5a.jpg"
+  },
+  {
+    "revision": "e9925c5e0e7df439bcd4414186bd17a3",
+    "url": "/sign-in/static/media/irislogo.e9925c5e.png"
+  },
+  {
+    "revision": "13a950331f4cf632c20c270941a1193b",
+    "url": "/sign-in/static/media/logo.13a95033.png"
+  }
+]);
